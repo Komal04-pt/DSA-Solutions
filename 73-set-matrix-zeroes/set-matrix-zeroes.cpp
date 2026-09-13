c82828c924f74f82aeb5f426f8f9e573dfc202f4
KOMAL PATEL <komal104btcse23@igdtuw.ac.in>
2026-09-13 12:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (77.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,24 +3,36 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
-
-        vector<bool> rowZero(row, false);
-        vector<bool> colZero(col, false);
+        bool rowZero = false;
 
         for(int r=0; r<row; r++){
             for(int c=0; c<col; c++){
                 if(matrix[r][c]==0){
-                    rowZero[r] = true;
-                    colZero[c] = true;
+                    matrix[0][c] = 0;
+                if(r>0){
+                    matrix[r][0] = 0;
+                }else{
+                    rowZero = true;
+                    }
                 }
             }
         }
 
-        for(int r=0; r<row; r++){
-            for(int c=0; c<col; c++){
-                if(rowZero[r] || colZero[c]){
+        for(int r=1; r<row; r++){
+            for(int c=1; c<col; c++){
+                if(matrix[r][0]==0 || matrix[0][c]==0){
                     matrix[r][c]=0;
                 }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int r=0; r<row; r++){
+                    matrix[r][0] = 0;
+                }
+            }
+        if(rowZero){
+            for(int c=0; c<col; c++){
+                matrix[0][c] = 0;
             }
         }
     }
