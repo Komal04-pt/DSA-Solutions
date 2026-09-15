62583cd7e67b30688b22f99cb11737881211c288
KOMAL PATEL <komal104btcse23@igdtuw.ac.in>
2026-09-15 21:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.70%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -15,14 +15,9 @@ public:
                 leftMin -= 1;
                 leftMax += 1;
             }
-
-            if(leftMax < 0){
-                 return false;
-            }
-            if(leftMin < 0){
-                leftMin = 0;
-            }
+            if(leftMax < 0) return false;
+            if(leftMin < 0) leftMin = 0;
         }
-        return leftMin == 0;;
+        return (leftMin==0);
     }
 };
